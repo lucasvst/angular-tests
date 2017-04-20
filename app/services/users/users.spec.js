2a4ec1589c00065ad50db1597b56ba2aa934832a
Lucas Teles <lucas.teles@fluig.com>
2017-04-20 13:12:47 -0300

    code style updated

--- app/services/users/users.spec.js
+++ app/services/users/users.spec.js
@@ -2,28 +2,10 @@ describe('Users factory', function() {
 
 	var Users,
 		userList = [
-			{
-				id: 1,
-				name: 'Lucas',
-				role: 'Programmer',
-				location: 'Joinville',
-				twitter: 'lucasvst'
-			},
-			{
-				id: 2,
-				name: 'John',
-				role: 'Architect',
-				location: 'Madrid',
-				twitter: 'johndoe'
-			}
-		],
-		singleUser = {
-			id: 2,
-			name: 'John',
-			role: 'Architect',
-			location: 'Madrid',
-			twitter: 'johndoe'
-		}
+            {id: 1, name: 'Lucas', role: 'Programmer', location: 'Joinville', twitter: 'lucasvst'},
+            {id: 2, name: 'John', role: 'Architect', location: 'Madrid', twitter: 'johndoe'}
+        ],
+		singleUser = {id: 2, name: 'John', role: 'Architect', location: 'Madrid', twitter: 'johndoe'}
 	;
 
 	beforeEach(angular.mock.module('api.users'));
